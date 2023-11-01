1ae7a19de204db673e519d5743acb4f62579f7bb
Dheison Gomes <dheisomgomes0@gmail.com>
2023-11-01 18:47:16 -0300

    Search is working

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 from os import cpu_count
 
-from sanic import Sanic
+from sanic import Sanic, redirect
 
 from api import PORT, routes
 
-server = Sanic(__name__)
+server = Sanic(__name__, strict_slashes=False)
+server.add_route(lambda _: redirect("/web/"), "/")
+server.static("/web", "web", index="index.html", strict_slashes=False)
 routes.add_all(server)
 
-if __name__ == '__main__':
-    server.go_fast(host='0.0.0.0', port=PORT, workers=cpu_count() or 1)
+if __name__ == "__main__":
+    server.go_fast(host="0.0.0.0", port=PORT, workers=cpu_count() or 1)
--- web/main.js
+++ web/main.js
@@ -7,14 +7,51 @@ const results_container = document.querySelector('.search-results')
 const hidden_element = (e) => e.classList.add('hidden')
 const show_element = (e) => e.classList.remove('hidden')
 
+const ResponseItem = ({ title, thumbnail, path }) => {
+  return `
+  <div class="response">
+    <a href="/web/download.html?path=${path}">
+      <div><img src="${thumbnail}" /></div>
+      <span class="flex">${title}</span>
+    </a>
+  </div>`
+}
+
+const ResponseContainer = ({ title, children }) => {
+  return `
+  <h2 class="server-title">${title}</h2>
+  <div class="server-response flex">${children}</div>`
+}
+
 query.focus()
 
 search.onclick = () => {
+  if (query.value.trim() === '') {
+    alert('Você não pode fazer uma pesquisa vazia!')
+    return
+  }
+
+  const onResponse = async (data) => {
+    const html = data.map(extractor_result => {
+      if (extractor_result.error || extractor_result.response.results.length === 0) return
+      const responseItems = extractor_result.response.results.map(ResponseItem)
+      return ResponseContainer({ title: extractor_result.extractor_id, children: responseItems.join("\n") })
+    })
+    if (html.length === 0) {
+      hidden_element(loading_animation)
+      show_element(empty_results)
+      return
+    }
+    results_container.innerHTML = html.join("\n")
+    hidden_element(loading_animation)
+    show_element(results_container)
+  }
+
   hidden_element(empty_results)
   hidden_element(results_container)
   show_element(loading_animation)
-  fetch('/api/search/all?query=' + query.value)
+  fetch('/api/search/all?query=' + query.value.trim())
     .then(r => r.json())
-    .then(r => console.log(r))
+    .then(onResponse)
     .catch(e => console.error(e))
 }
